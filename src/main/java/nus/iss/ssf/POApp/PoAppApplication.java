ff6f0b3a871b434ebbecb722ee48fc3ab887e089
lyon-tan96 <lyontsy@gmail.com>
2022-03-25 09:11:07 +0800

    new

---
+++ src/main/java/nus/iss/ssf/POApp/PoAppApplication.java
@@ -0,0 +1,13 @@
+package nus.iss.ssf.POApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nus/iss/ssf/POApp/PoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.iss.ssf.POApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
